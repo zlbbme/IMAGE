bb3115e01bd04ed64aa17a0850518035c5758385
zlbbme <zlbbme@sina.com>
2024-01-23 20:57:06 +0800

    mha_modifed

--- mha_conversion/mha_conversion.py
+++ mha_conversion/mha_conversion.py
@@ -53,26 +53,26 @@ def convert_mha_to_npy(mha_file, npy_path):
         #保存为mha文件
         np.save(npy_path+'/'+str(i+1)+'.npy',img)
 
-def convert_mha_dicom(mha_file, dicom_path,dcm_file):
-    # Read the existing DICOM file
-    ds = pydicom.dcmread(dcm_file, force=True)
-    # Read the original mha file
-    image = sitk.ReadImage(mha_file)
-    img_data = sitk.GetArrayFromImage(image)
-    #输出img_data的类型
-    print(img_data.dtype)
-    height = img_data.shape[0]
-    width = img_data.shape[2]
-    channel = img_data.shape[1]
-    if not os.path.exists(dicom_path):
-        os.makedirs(dicom_path)
+# def convert_mha_dicom(mha_file, dicom_path,dcm_file): #3d Slicer可完成此工作
+#     # Read the existing DICOM file
+#     ds = pydicom.dcmread(dcm_file, force=True)
+#     # Read the original mha file
+#     image = sitk.ReadImage(mha_file)
+#     img_data = sitk.GetArrayFromImage(image)
+#     #输出img_data的类型
+#     print(img_data.dtype)
+#     height = img_data.shape[0]
+#     width = img_data.shape[2]
+#     channel = img_data.shape[1]
+#     if not os.path.exists(dicom_path):
+#         os.makedirs(dicom_path)
 
-    for i in range(channel):
-        img = np.zeros((height,width), dtype=np.int16)
-        img = img_data[:,i,:]
-        print(img.shape)
+#     for i in range(channel):
+#         img = np.zeros((height,width), dtype=np.int16)
+#         img = img_data[:,i,:]
+#         print(img.shape)
 if __name__ == '__main__':
-    #convert_mha_to_npy('CT_01.mha', './npy')
+    convert_mha_to_npy('CT_01.mha', './npy')
     #convert_mha_to_png('CT_01.mha','./png')
     # img_test = sitk.ReadImage('CT_01.mha')        #[x,y,z]
     # img_data = sitk.GetArrayFromImage(img_test)   #[z,y,x]
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     #convert_mha_to_png('CT_01_out.mha','./png2')
 
     #convert_mha_to_png('CT_01.mha','./png')
-    convert_mha_dicom('CT_01.mha','./dcm','2023021310_CT20_image00000.DCM')
+    # convert_mha_dicom('CT_01.mha','./dcm','2023021310_CT20_image00000.DCM')
         #获取dicom文件的tag
     #ds = sitk.ReadImage('./dcm/1.dcm')
     # #打印所有tag
--- mha_conversion/mha_registration.py
+++ mha_conversion/mha_registration.py
@@ -1,27 +1,41 @@
 import SimpleITK as sitk
 import numpy as np
 
-# 读取第一个.mha图像
-fixed_image = sitk.ReadImage("CT_01.mha",outputPixelType=sitk.sitkFloat32)
+def mha_to_equal(input_mha,fixed_mha,output_mha):
+    # 读取第一个.mha图像
+    fixed_image = sitk.ReadImage(fixed_mha,outputPixelType=sitk.sitkFloat32)
+    #print('fixed_image',fixed_image.GetSize())
+    moving_image = sitk.ReadImage(input_mha,outputPixelType=sitk.sitkFloat32)
+    #print('moving_image',moving_image.GetSize())
+    # 创建配准对象
+    registration_method = sitk.ImageRegistrationMethod()
 
-# 读取第二个.mha图像
+    # 设置配准方法和参数
+    registration_method.SetMetricAsMeanSquares()
+    registration_method.SetOptimizerAsRegularStepGradientDescent(learningRate=0.1, minStep=1e-4, numberOfIterations=1)#只迭代一次
+    registration_method.SetInitialTransform(sitk.TranslationTransform(fixed_image.GetDimension()))
 
-#convert_mha_direction("FDK4D_01.mha", "FDK4D_01_out.mha")
-moving_image = sitk.ReadImage("FDK4D_01.mha",outputPixelType=sitk.sitkFloat32)
+    # 执行配准
+    transform = registration_method.Execute(fixed_image, moving_image)
 
-# 创建配准对象
-registration_method = sitk.ImageRegistrationMethod()
+    # 应用变换到移动图像
+    registered_image = sitk.Resample(moving_image, fixed_image, transform, sitk.sitkLinear, 0.0)
 
-# 设置配准方法和参数
-registration_method.SetMetricAsMeanSquares()
-registration_method.SetOptimizerAsRegularStepGradientDescent(learningRate=0.1, minStep=1e-4, numberOfIterations=100)
-registration_method.SetInitialTransform(sitk.TranslationTransform(fixed_image.GetDimension()))
+    # 保存配准后的图像，确保和fixed_image具有相同的原点、方向和间距和维度
+    sitk.WriteImage(registered_image, output_mha)
+    #打印维度变化
+    print('from',moving_image.GetSize(),'to',registered_image.GetSize())
 
-# 执行配准
-transform = registration_method.Execute(fixed_image, moving_image)
 
-# 应用变换到移动图像
-registered_image = sitk.Resample(moving_image, fixed_image, transform, sitk.sitkLinear, 0.0)
+if __name__ == '__main__':
+    # for i in range(10):
+    #     j = i+1
+    #     input_mha = r'E:\dataset\2018sydney\P3\MC_T_P3_LD\FDKRecon\FDK4D_'+'%02d'%j +'.mha'
+    #     fixed_mha = r'E:\dataset\2018sydney\P3\MC_T_P3_Prior\CT_'+'%02d'%j +'.mha'
+    #     output_mha = r'E:\dataset\temp_mha\CBCTp'+'%d'%j +'.mha'
+    #     mha_to_equal(input_mha,fixed_mha,output_mha)
 
-# 保存配准后的图像，确保和fixed_image具有相同的原点、方向和间距和维度
-sitk.WriteImage(registered_image, "registered_image.mha")
+    input_mha = r'E:\dataset\2018sydney\P3\MC_T_P3_LD\FDKRecon\FDK3D.mha'
+    fixed_mha = r'E:\dataset\2018sydney\P3\MC_T_P3_Prior\CT_01.mha'
+    output_mha = r'E:\dataset\temp_mha\P3\CBCTprior.mha'
+    mha_to_equal(input_mha,fixed_mha,output_mha)
