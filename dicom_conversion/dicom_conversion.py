e85bc306d9fb368e5637f6b77cded8014bb20c88
zlbbme <zlbbme@sina.com>
2024-01-25 17:19:16 +0800

    dicom转换修改

--- dicom_conversion/dicom_conversion.py
+++ dicom_conversion/dicom_conversion.py
@@ -43,7 +43,7 @@ def convert_dicom_to_npy(dicom_folder, output_folder):
     #判断输出文件夹是否存在，不存在则创建
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-    _,_,slice_num  = dicom_read_max_min(dicom_folder)
+    min_CT_num,max_CT_num,slice_num  = dicom_read_max_min(dicom_folder)
     #遍历dicom文件夹下的所有文件，如果是dicom文件则转换为png文件
     for root, dirs, files in os.walk(dicom_folder):
 
@@ -58,6 +58,8 @@ def convert_dicom_to_npy(dicom_folder, output_folder):
             instance_number = ds.InstanceNumber
             #将数据格式转换为numpy
             pixel_array = ds.pixel_array.astype(np.int16)
+
+            pixel_array = (pixel_array - min_CT_num) / (max_CT_num - min_CT_num) * 4000   #归一到0-4000
             #保存为npy文件
             np_filename = str(slice_num-instance_number) + '.npy'
             np_filepath = os.path.join(output_folder, np_filename)
@@ -106,7 +108,7 @@ def dicom_read_max_min(dicom_path):
                 if file.endswith(".DCM"):
                     len_dicom += 1
                     dicom_path = os.path.join(root, file)
-                    print(dicom_path)
+                    #print(dicom_path)
                 # 读取 DICOM 文件
                 ds = pydicom.dcmread(dicom_path, force=True)
                 ds.file_meta.TransferSyntaxUID = pydicom.uid.ImplicitVRLittleEndian
@@ -116,23 +118,61 @@ def dicom_read_max_min(dicom_path):
                 max_list.append(np.max(pixel_array))
                 min_list.append(np.min(pixel_array))
                 
-    max_CT_num = np.max(max_list)
-    min_CT_num = np.min(min_list)
+    max_CT_num = np.max(max_list) ; min_CT_num = np.min(min_list)
     return min_CT_num, max_CT_num, len_dicom
+
+def normalize_image_intensity(image, min_val, max_val):
+    image = image.astype(np.float32)
+    min_image = np.min(image)
+    max_image = np.max(image)
+    normalized_image = (image - min_image) / (max_image - min_image)  # 归一化到0-1
+    normalized_image = normalized_image * (max_val - min_val) + min_val  # 缩放到min_val-max_val
+    return normalized_image.astype(np.int16)
+
+def normalize_dicom_intensity(dicom_path, min_val, max_val):
+    for dicom_files in os.listdir(dicom_path):
+        for dicom_file in dicom_files:
+            if dicom_file.endswith(".DCM"):
+                dicom_file_path = os.path.join(dicom_path, dicom_files)
+                print(dicom_path)
+    # 读取 DICOM 文件
+                ds = pydicom.dcmread(dicom_file_path, force=True)
+                ds.file_meta.TransferSyntaxUID = pydicom.uid.ImplicitVRLittleEndian
+                #获取dicom文件的instance number
+                instance_number = ds.InstanceNumber
+                pixel_array = ds.pixel_array
+                # 归一化图像
+                normalized_pixel_array = normalize_image_intensity(pixel_array, 0, 4000)
+                # 更新 DICOM 文件的像素数据
+                ds.PixelData = normalized_pixel_array.tobytes()
+                ds.Rows, ds.Columns = normalized_pixel_array.shape
+                ds.PixelRepresentation = 1
+                ds.BitsStored = 16
+                ds.BitsAllocated = 16
+                ds.HighBit = 15
+                ds.SamplesPerPixel = 1
+                ds.PhotometricInterpretation = "MONOCHROME2"
+                ds.PixelData = normalized_pixel_array.tobytes()
+                # 保存修改后的 DICOM 文件
+                ds.save_as(dicom_file_path)
+    
 
 if __name__ == "__main__":
     # Usage example
-    dicom_folder = r'E:\dataset\temp_dicom\100HM10395'
-    output_folder = 'png'    
+    dicom_folder = r'E:\dataset\temp_dicom\100HM10395\CBCTp0'
+    output_folder = 'npy'    
     #convert_dicom_to_png(dicom_folder, output_folder)
     #convert_dicom_to_npy(dicom_folder, output_folder)
     #dicom_series_to_nrrd(dicom_folder, output_folder)
     #读取npy文件并显示
-    # data = np.load("./npy/39.npy")
-    # #可视化显示
-    # plt.imshow(data, cmap='gray')
-    # plt.show()
+    data = np.load('./npy/45.npy')
+    #可视化显示
+    plt.imshow(data, cmap='gray')
+    plt.show()
     #convert_dicom_to_mha(dicom_folder, output_folder)
     #convert_dicom_to_nii(dicom_folder, output_folder)
-    min_CT_num, max_CT_num, len_dicom =dicom_read_max_min(dicom_folder)
-    print(max_CT_num, min_CT_num)
+    # min_CT_num, max_CT_num, len_dicom =dicom_read_max_min(dicom_folder)
+    # print(max_CT_num, min_CT_num)
+    # normalize_dicom_intensity(dicom_folder, 0, 4000)
+    # min_CT_num, max_CT_num, len_dicom =dicom_read_max_min(dicom_folder)
+    # print(max_CT_num, min_CT_num)
