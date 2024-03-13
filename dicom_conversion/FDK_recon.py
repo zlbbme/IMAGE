66a12e74f8abde9f663b4068351d716f39ba1556
zlbbme <zlbbme@sina.com>
2024-03-13 16:19:08 +0800

    3.13

--- dicom_conversion/FDK_recon.py
+++ dicom_conversion/FDK_recon.py
@@ -14,7 +14,8 @@ from skimage.metrics import structural_similarity as SSIM
 def read_dicom(path_to_dicom_file):
     ds = pydicom.dcmread(path_to_dicom_file, force=True)
     ds.file_meta.TransferSyntaxUID = pydicom.uid.ImplicitVRLittleEndian
-    image_data = ds.pixel_array.astype(np.float32)  # 转换为浮点类型
+    instance_number = ds.InstanceNumber
+    image_data = ds.pixel_array.astype(np.int16)  # 转换为浮点类型
     return image_data
 
 #浮点数扩维并拼接
@@ -43,50 +44,69 @@ if __name__ == '__main__':
     gate_path = patient_file + '\CTp0'
     #get gated image data
     gate_data = read_gate(gate_path) # [512,512,60]
-    print(gate_data.shape)
-    # 定义重建空间尺寸
-    volume_shape = gate_data.shape
-
+    print('input_data_shape:',gate_data.shape)
+    print('data range from %d to %d'%(gate_data.min(),gate_data.max()))
     # 定义重建空间和投影几何
-    #reco_space = odl.uniform_discr([-128, -128, -128], [128, 128, 128], volume_shape)
-    space = tomo.elekta_xvi_space(shape= volume_shape)
+    space = tomo.elekta_xvi_space(shape= gate_data.shape)
     detector_geometry = tomo.elekta_xvi_geometry(angles = np.linspace(0, 2 * np.pi, 60, endpoint=False),#num_angles=600,
                                                 piercing_point=(512.0, 512.0),detector_shape=(1024, 1024))
-    print(detector_geometry.detector.shape)
+    print('detector.shape:',detector_geometry.detector.shape)     # (1024, 1024)
 
     # Create ray transform
     ray_transform = odl.tomo.RayTransform(space, detector_geometry, use_cache=False)
     # Create artificial data
-    projections = ray_transform(gate_data)
-    print(projections.dtype)
-    print(projections.shape)
-    
+    projections = ray_transform(gate_data)     # (60, 1024, 1024), 60 projections ,float32
 
     # Get default FDK reconstruction operator
     recon_op = tomo.elekta_xvi_fbp(ray_transform)
 
-    reconstruction = recon_op(projections)
+    reconstruction = recon_op(projections).astype(np.int16)
+    #转换成numpy数组
+    reconstruction = reconstruction.asarray()
+    reconstruction[reconstruction <0] = 0 
+    #归一到0-最大值
+    #reconstruction = (reconstruction - reconstruction.min())/(reconstruction.max() - reconstruction.min())*4000
 
-    print(reconstruction.dtype)
-    print(reconstruction.shape)
-
-
-    projections.show('projections')
+    print(reconstruction.shape)  # (512, 512, 60) int16
+    #打印最大值
+    print('data range from %d to %d'%(reconstruction.min(),reconstruction.max()))
+    #projections.show('projections')  #显示投影正弦图
     
     #通过512*512的灰度图片显示gate_date的三个方向的投影
 
-    plt.imshow(gate_data[:,:,24],cmap='gray')
+    # plt.imshow(gate_data[:,:,24],cmap='gray')
+    # plt.show()
+
+    # plt.imshow(reconstruction[:,:,24],cmap='gray')
+    # plt.show()
+    #两幅图像并列显示
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(gate_data[:,:,24],cmap='gray')
+    ax[1].imshow(reconstruction[:,:,24],cmap='gray')
     plt.show()
 
-    plt.imshow(reconstruction[:,:,24],cmap='gray')
-    plt.show()
-    
-    hist, bins = np.histogram(gate_data[:,:,24], bins=50)  # bins参数定义了直方图的粒度
+    hist, bins = np.histogram(gate_data[:,:,24], bins=50)
     #打印gate_data[:,:,24]的灰度直方图
     plt.hist(gate_data[:,:,24].flatten(), bins=bins)
     plt.show()
-
-
-    plt.hist(reconstruction[:,:,24].asarray().flatten(), bins=bins)
+    plt.hist(reconstruction[:,:,24].flatten(), bins=bins)
     plt.show()
+    
+    # #两幅图像显示灰度直方图
+    # fig, ax = plt.subplots(1, 2)
+    # ax[0].hist(gate_data[:,:,24].flatten(), bins=50)
+    # ax[0].set_title('Original')
+    # ax[1].hist(reconstruction[:,:,24].asarray().flatten(), bins=50)
+    # ax[1].set_title('Reconstruction')
+    # plt.show()
+    
+    #计算均方误差
+    mse = np.mean((gate_data[:,:,24] - reconstruction[:,:,24])**2)
+    print(mse)
+    #计算峰值信噪比
+    psnr = PSNR(gate_data[:,:,24],reconstruction[:,:,24],data_range=gate_data[:,:,24].max() - gate_data[:,:,24].min())
+    print(psnr)
+    #计算结构相似度
+    ssim = SSIM(gate_data[:,:,24],reconstruction[:,:,24],data_range=gate_data[:,:,24].max() - gate_data[:,:,24].min())
+    print(ssim)
     
---
+++ image_metrics/reonstruction_metrics.py
@@ -0,0 +1,43 @@
+from skimage.metrics import peak_signal_noise_ratio 
+from skimage.metrics import structural_similarity as ssim1
+from skimage.metrics import mean_squared_error
+
+import numpy as np
+
+def compare(recon0, recon1, verbose=True):
+    #mse_recon = mean_squared_error(recon0, recon1)
+    #均方根
+    mse_recon = np.sqrt(np.mean((recon0-recon1)**2))
+    # np.mean((recon0-recon1)**2)
+
+    small_side = np.min(recon0.shape)
+    if small_side < 7:
+        if small_side % 2:  # if odd
+            win_size = small_side
+        else:
+            win_size = small_side - 1
+    else:
+        win_size = None
+
+    ssim_recon = ssim1(recon0, recon1,
+                       data_range=recon0.max() - recon0.min(), win_size=win_size)
+
+    psnr_recon = peak_signal_noise_ratio(recon0, recon1,
+                                         data_range= recon0.max() - recon0.min())#recon0.max() - recon1.min())  #data_range的大小影响PSNR的大小
+
+    if verbose:
+        err_string = 'MSE: {:.8f}, SSIM: {:.3f}, PSNR: {:.3f}'
+        print(err_string.format(mse_recon, ssim_recon, psnr_recon))
+    return (mse_recon, ssim_recon, psnr_recon)
+
+
+
+if __name__ == '__main__':
+    recon0_path = r'/Data/SaveBibMip-SX/eva_data/PRIOR_test/Sparse/phase_1/sparse_040.npy'
+    recon1_path = r'/Data/SaveBibMip-SX/eva_data/PRIOR_test/Label/phase_1/label_040.npy'
+    recon0 = np.load(recon0_path)
+    print(recon0.max(),recon0.min())
+    recon1 = np.load(recon1_path)
+    print(recon1.max(),recon1.min())
+    mse_recon, ssim_recon, psnr_recon = compare(recon0, recon1) 
+    print(recon0_path,mse_recon, ssim_recon, psnr_recon)
